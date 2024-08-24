4809495010c9d48fe560c7cac6273f4d47eb0dc7
Khitish-ztx <khitish152006@gmail.com>
2024-08-24 21:22:08 +0530

    config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
+    OWNER_ID = "1647602447"
+    sudo_users = "1647602447"
+    GROUP_ID = -1002210286036
+    TOKEN = "7452110956:AAE3DCldVOdNpBimfS0G6-LxvsHIlBES1Rk"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "@Anime_Chat_Group_International"
+    UPDATE_CHAT = "@Anime_Chat_Group_International"
+    BOT_USERNAME = "itachigrab_bot"
+    CHARA_CHANNEL_ID = "-1002210286036"
+    api_id = 22257865
+    api_hash = "97fc23c20e06f411abbf36cad8e118ce"
 
     
 class Production(Config):
